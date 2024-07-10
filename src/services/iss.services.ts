fcb8f0a650c3a296838e8cd34758c721638dbff6
Yair Avivi <yavivi441@gmail.com>
2024-07-10 13:23:23 +0300

    split functions

--- src/services/iss.services.ts
+++ src/services/iss.services.ts
@@ -3,12 +3,16 @@ import { getCountries } from "./countries.services";
 import { isPointInPolygon } from "../utils/geometry.utils";
 import { IssLocation } from "../models/issLocation.models";
 
-export async function getIssCountry(): Promise<string> {
+export async function getIssCoordinates() {
   const { data } = await axios.get("http://api.open-notify.org/iss-now.json");
-  const issLocation: IssLocation = {
+  return {
     latitude: parseFloat(data.iss_position.latitude),
     longitude: parseFloat(data.iss_position.longitude),
-  };
+  } as IssLocation;
+}
+
+export async function getIssCountry(): Promise<string> {
+  const issLocation = await getIssCoordinates();
 
   const countries = await getCountries();
   for (const country of countries) {
