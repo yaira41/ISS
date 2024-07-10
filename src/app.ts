4ec95efc87cd0a3b7aa4856242fd235ffa1f6475
Yair Avivi <yavivi441@gmail.com>
2024-07-10 14:18:42 +0300

    use utm route

--- src/app.ts
+++ src/app.ts
@@ -1,8 +1,7 @@
 import express from "express";
 import swaggerJsdoc from "swagger-jsdoc";
 import swaggerUi from "swagger-ui-express";
-import countriesRoute from "./routes/countries.routes";
-import issRoute from "./routes/iss.routes";
+import { countriesRoute, issRoute, utmRoute } from "./routes";
 import swaggerOptions from "./config/swagger";
 
 const app = express();
@@ -14,5 +13,6 @@ app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(specs));
 
 app.use("/api/countries", countriesRoute);
 app.use("/api/ISS", issRoute);
+app.use("/api/UTM", utmRoute);
 
 export default app;
