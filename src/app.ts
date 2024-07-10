c67c891700682e83236ad22e579ad8983dc58d21
Yair Avivi <yavivi441@gmail.com>
2024-07-10 12:02:44 +0300

    use swagger

--- src/app.ts
+++ src/app.ts
@@ -1,7 +1,13 @@
 import express from "express";
+import swaggerJsdoc from "swagger-jsdoc";
+import swaggerUi from "swagger-ui-express";
+import swaggerOptions from "./config/swagger";
 
 const app = express();
 
 app.use(express.json());
+
+const specs = swaggerJsdoc(swaggerOptions);
+app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(specs));
 
 export default app;
